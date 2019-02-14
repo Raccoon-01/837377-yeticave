3d5e1393397254b392c2767b2432bc907c29d279
Raccoon <royal-01@mail.ru>
2019-02-15 00:54:54 +0600

    исправлена функция для форматирования цены лота

--- index.php
+++ index.php
@@ -57,28 +57,13 @@ $sixth_item = [
 $item_list = [$first_item , $second_item, $third_item, $fourth_item, $fifth_item, $sixth_item];
 $item_count = count($item_list);
 
-/*
-function sum_format (int $num_format) {
-    if ($num_format > 1000) {
-        number_format(100000, 0, '.', " ");
-        ceil($num_format);
+
+function price_format (float $item_price) {
+    $round = ceil($item_price);
+    if ($round > 1000) {
+        $price_format = number_format($round, 0, '.', " ");
     }
-    return $num_format . " ₽";
-}*/
-
-function sum_format ($item_list) {
-    $numbers = str_split($item_list);
-    array_walk($numbers,
-        function(&$value) {
-        if($value > 999) {
-            $value = number_format($value, 2, ',', ' ');
-            ceil ($value);
-        }
-        }
-    );
-    $result = implode("", $numbers);
-
-    return $result .= " ₽";
+    return $price_format .= " ₽";
 }
 ?>
 
@@ -156,7 +141,7 @@ function sum_format ($item_list) {
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?php echo sum_format($value["price"]);
+                            <span class="lot__cost"><?php echo price_format($value["price"]);
                             ?></span>
                         </div>
                         <div class="lot__timer timer">
