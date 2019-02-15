95b1145dd711328523aee81f158303533aebdca0
Raccoon <royal-01@mail.ru>
2019-02-15 18:41:39 +0600

    шаблонизация php кода

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php function include_template($name, $data) {
+$name = 'templates/' . $name;
+$result = '';
+
+if (!is_readable($name)) {
+return $result;
+}
+
+ob_start();
+extract($data);
+require $name;
+
+$result = ob_get_clean();
+
+return $result;
+};
+
+
+
+function price_format (float $item_price) {
+    $round = ceil($item_price);
+    if ($round > 1000) {
+        $price_format = number_format($round, 0, '.', " ");
+    }
+    return $price_format .= " ₽";
+};
+?>
